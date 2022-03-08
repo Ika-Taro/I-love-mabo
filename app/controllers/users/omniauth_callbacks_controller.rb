bb3ecf27a1475cfdc052dbe1d1321d7f95eb8501
Ika-Taro <black.jkh@gmail.com>
2022-03-08 16:54:44 +0900

    SNS認証実装

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def google_oauth2
+    authorization
+  end
+  
+  private
+
+  def authorization
+    sns_info = User.from_omniauth(request.env["omniauth.auth"])
+    @user = sns_info[:user]
+
+    if @user.persisted? 
+      sign_in_and_redirect @user, event: :authentication
+    else
+      @sns_id = sns_info[:sns].id
+      render template: 'devise/registrations/new'
+    end
+  end
+end
